e37fe7e11fe51f9ebb4ce5e5f4ba2d8b053f684f
momzo fall <momzo12@optimum.net>
2020-01-28 09:38:48 -0500

    Initial Source

---
+++ zipdeploy.ps1
@@ -0,0 +1,8 @@
+$username = "cercorp"
+$password = "All7even"
+$filePath = "C:\Code\AzureDeploy\website.zip"
+$apiUrl = "https://ourwebapp.scm.azurewebsites.net/api/zipdeploy"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
+$userAgent = "powershell/1.0"
+
+Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method POST -InFile $filePath -ContentType "multipart/form-data"
